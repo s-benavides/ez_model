8a07d778514cc7524e5aa0a878a85020c2f8de59
Santiago Benavides <santib@mit.edu>
2021-01-14 11:32:23 -0500

    Added example jobscript file.

---
+++ example_run_dir/jobscript.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Partition             Nodes   S-C-T   Timelimit
+# ---------             -----   -----   ---------
+# sched_mit_hill        (32)    2-8-1   12:00:00
+# sched_mit_raffaele    (32)    2-10-1  12:00:00
+# sched_any_quicktest   2       2-8-1   00:15:00
+# newnodes              (32)    2-10-1  12:00:00
+
+# Job
+#SBATCH --partition=sched_mit_hill
+##SBATCH --partition=sched_any_quicktest
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=5:05:00 ## Extra 5 mins to do last saves
+#SBATCH -J ez_400x80  # sensible name for the job
+
+# Setup conda and dedalus environment
+minicondahome="/home/santiago_b/miniconda3"
+. $minicondahome/etc/profile.d/conda.sh
+##source ~/dedalus/dedalus_modules
+conda activate ez
+
+# Run scripts
+mpiexec -n 16 python3 main_q_parallel.py
